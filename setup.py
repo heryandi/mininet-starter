fc20ed858b98be6483692b4d5d50a66a1948f357
Heryandi <heryandi@gmail.com>
2013-09-13 14:56:07 +0800

    Add classifier and keyword to metadata

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@ import os
 import mnp.patch
 from setuptools import setup, find_packages
 
-scripts = [os.path.join('bin', filename) for filename in os.listdir('bin')] 
+scripts = [os.path.join("bin", filename) for filename in os.listdir("bin")] 
 
 setup(
-    name='mininet-starter',
-    version='1.0.0',
-    author='Heryandi',
-    author_email='Heryandi@gmail.com',
-    packages=find_packages(exclude='test'),
+    name="mininet-starter",
+    version="1.0.0",
+    author="Heryandi",
+    author_email="Heryandi@gmail.com",
+    packages=find_packages(exclude="test"),
     scripts=scripts,
-    url='https://github.com/heryandi/mininet-starter',
-    license='MIT',
-    description='Starter package to develop new Mininet modules',
-    long_description=open('README.rst').read(),
+    url="https://github.com/heryandi/mininet-starter",
+    license="MIT",
+    description="Starter package to develop new Mininet modules",
+    long_description=open("README.rst").read(),
 	data_files=[
 		("share/doc/mininet-starter", ["doc/USAGE.txt"])
 	],
@@ -25,8 +25,13 @@ setup(
         "pip",
         "setuptools",
     ],
-    entry_points={'console_scripts': [
-        'starter_console_script_1 = your_source_code:main',
-        'starter_console_script_2 = your_source_code.utils:do_something',
-    ]}
+    entry_points={"console_scripts": [
+        "starter_console_script_1 = your_source_code:main",
+        "starter_console_script_2 = your_source_code.utils:do_something",
+    ]},
+    classifiers=[
+        "Mininet :: Example",
+        "Mininet :: Experiment",
+    ],
+    keywords="mininet example documentation guide sample tutorial"
 )
